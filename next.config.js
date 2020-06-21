c862eb1569b468f431d164a85e6ba7e2a5599ffe
Justin Gaba <gabalicious113@gmail.com>
2020-06-20 23:46:18 -0400

    Fix for prod

--- next.config.js
+++ next.config.js
@@ -1,11 +1,3 @@
 const withSass = require("@zeit/next-sass");
-const withFonts = require("next-fonts");
 
-module.exports = withFonts(
-  withSass({
-    webpack(config, options) {
-      // custom webpack loaders if you need
-      return config;
-    },
-  })
-);
+module.exports = withSass();
